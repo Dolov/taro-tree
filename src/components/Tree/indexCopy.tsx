ac7243bbec267d0e157157b094c5ee0a798f70b9
shisongyan <songyan_shi@163.com>
2019-09-09 15:43:14 +0800

    ✨tree

---
+++ src/components/Tree/indexCopy.tsx
@@ -0,0 +1,32 @@
+import Taro from '@tarojs/taro'
+import { View, Text } from '@tarojs/components'
+import TreeCopy from './index'
+
+interface ITree {
+	dataSource: [];
+}
+
+export default class Tree extends Taro.PureComponent<ITree> {
+
+	state = {
+
+	}
+
+	render() {
+		const { dataSource } = this.props
+		if (!Array.isArray(dataSource)) return null
+		return (
+			<View>
+				{dataSource.map(item => {
+					const { value, label, children } = item
+					return (
+						<View key={value} style={{paddingLeft:'30px'}}>
+							<Text>{label}</Text>
+							<TreeCopy dataSource={children} />
+						</View>
+					)
+				})}
+			</View>
+		)
+	}
+}
--- taro-tree/README.md
+++
@@ -1,2 +0,0 @@
-# taro-tree
-基于 taro 的 Tree 组件，可用于小程序、h5
